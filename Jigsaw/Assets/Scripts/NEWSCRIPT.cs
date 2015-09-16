ff7b3c29b0a2e98e0e0673b647e72ab4762caae6
holy_pilgrim90@yahoo.com <holy_pilgrim90@yahoo.com>
2015-09-16 13:24:44 -0500

    Adding NEWSCRIPT that prints to the console

---
+++ Jigsaw/Assets/Scripts/NEWSCRIPT.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NEWSCRIPT : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("James is the best!!!");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
