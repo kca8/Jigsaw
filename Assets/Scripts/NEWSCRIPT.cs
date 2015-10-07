146968a3bdc78c7d651a5540740390c47c212b51
holy_pilgrim90@yahoo.com <holy_pilgrim90@yahoo.com>
2015-10-07 15:41:21 -0500

    Adding sentryBot and point light to main camera in all scenes

--- Assets/Scripts/NEWSCRIPT.cs
+++ Assets/Scripts/NEWSCRIPT.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class NEWSCRIPT : MonoBehaviour {
 
+	public GameObject stuff;
+
 	// Use this for initialization
 	void Start () {
 		print ("Kirby the Gigas");
---
+++ Assets/Scripts/Sentry.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Sentry : MonoBehaviour {
+
+	public Transform target; //the enemy's target
+	public float moveSpeed = 3; //move speed
+	public float rotationSpeed = 2; //speed of turning
+	
+	public int currentWaypoint = 0;
+	public Transform waypointParent;
+	public Transform waypoint_goto;
+	public List<Transform> waypointList;
+	
+	// Use this for initialization
+	void Start () {
+		//Transform[] Path = waypoint.GetComponentsInChildren<Transform>();
+		
+		foreach (Transform child in waypointParent){
+			waypointList.Add(child.transform);
+		}
+		
+		print("Size of waypointList = " + this.waypointList.Count);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(currentWaypoint < this.waypointList.Count)
+		{
+			if(waypoint_goto == null)
+				waypoint_goto = waypointList[currentWaypoint];
+			
+			walk();
+		}
+		
+		else
+		{
+			currentWaypoint = 0;
+			print ("Reseting Waypoints!\n");
+			waypoint_goto = waypointList[currentWaypoint];
+			print ("Current Waypoint = " + currentWaypoint + "\n");
+		}
+	}
+	
+	void walk(){
+		
+		// rotate towards the target
+		transform.forward = Vector3.RotateTowards(transform.forward, waypoint_goto.position - transform.position, moveSpeed*Time.deltaTime, 0.0f);
+		
+		// move towards the target
+		transform.position = Vector3.MoveTowards(transform.position, waypoint_goto.position, moveSpeed*Time.deltaTime);
+		
+		if(transform.position == waypoint_goto.position)
+		{
+			currentWaypoint ++;
+			waypoint_goto = waypointList[currentWaypoint];
+			print ("Reached waypoint " + currentWaypoint + "\n");
+			//print ("Path Length = " + this.Path.Length + "\n");
+		}
+		
+	}
+}
