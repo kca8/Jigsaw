fb791e9197dba6f42df6a6015bf45f3a81048484
holy_pilgrim90@yahoo.com <holy_pilgrim90@yahoo.com>
2015-10-11 12:05:33 -0500

    Updated Waypoints

--- Assets/Scripts/Sentry.cs
+++ Assets/Scripts/Sentry.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class Sentry : MonoBehaviour {
 
 	public Transform target; //the enemy's target
+	public float sight = 5.0f; //Sight radius of the agent
 	public float moveSpeed = 3; //move speed
 	public float rotationSpeed = 2; //speed of turning
 
@@ -12,6 +13,12 @@ public class Sentry : MonoBehaviour {
 	public Transform waypointParent; //The object containing a set of waypoint_goto's
 	Transform waypoint_goto; //The current waypoint objective
 	List<Transform> waypointList = new List<Transform>(); //An array of waypoint_goto's
+	public Waypoint thisWaypoint;
+	
+	public bool searchForObject = false;
+	
+	Vector3 origin;
+	
 	
 	// Use this for initialization
 	void Start () {
@@ -24,20 +31,37 @@ public class Sentry : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(currentWaypoint < this.waypointList.Count)
-		{
-			if(waypoint_goto == null)
-				waypoint_goto = waypointList[currentWaypoint];
+		
+		if(searchForObject == false)
+			if(currentWaypoint < this.waypointList.Count)
+			{
+				if(waypoint_goto == null)
+					waypoint_goto = waypointList[currentWaypoint];
+				
+				walk();
+			}
 			
-			walk();
-		}
+			else
+			{
+				currentWaypoint = 0;
+				waypoint_goto = waypointList[currentWaypoint];
+			}
 		
-		else
+		if (Input.GetKey(KeyCode.H))
 		{
-			currentWaypoint = 0;
-			waypoint_goto = waypointList[currentWaypoint];
+			foreach (Transform child in waypointParent) {
+				print(thisWaypoint.calcDistance(child.transform));
+			}
 		}
 		
+		if (Input.GetKey(KeyCode.J))
+		{
+			foreach (Transform child in waypointParent) {
+				print(thisWaypoint.calcDistanceToWaypoint(this.transform, child.transform));
+			}
+		}
+			//findPath(origin, sight, "Waypoint");
+			
 	}
 	
 	//-----------------------------------------------------------------
@@ -55,9 +79,33 @@ public class Sentry : MonoBehaviour {
 		
 		if(transform.position == waypoint_goto.position)
 		{
-			currentWaypoint ++;
+			currentWaypoint++;
 			waypoint_goto = waypointList[currentWaypoint];
 		}
 		
 	}
+	
+//	public static Vector3 findPath(Vector3 center, float radius, string refTag) {
+//		center.y = 0f;
+//		Vector3 detectTotal = Vector3.zero;	
+//		Vector3 fleeVector = Vector3.zero;
+//		bool detected = false;
+//		
+//		Transform[] seenObjects = Physics.OverlapSphere(center, radius); 
+//		for (int i = 0; i < seenObjects.Length; i++) {
+//			if(seenObjects[i].gameObject.transform.tag == refTag){ 
+//				detected = true;
+//				Vector3 detectCurrent = center - seenObjects[i].gameObject.transform.position; 
+//				detectCurrent.y = 0f; 
+//				detectTotal = detectCurrent + detectTotal;
+//			}
+//		}
+//		if (detected) {
+//			fleeVector = detectTotal;
+//			return fleeVector.normalized;
+//		}
+//		else {
+//			return Vector3.zero; 
+//		}
+//	}
 }
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour {
+	
+	public Transform goal;
+	float distance = 0.0f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey(KeyCode.G))
+		{
+			distance = calcDistance();
+			printDistance();
+		}
+	}
+	
+	public void printDistance() {
+		print("Distance from Goal: " + distance + "\n");
+	}
+	
+	private float calcDistance() {
+		float distance2 = Vector3.Distance(transform.position, goal.position);
+		
+		return distance2;
+	}
+	
+	public float calcDistance(Transform origin) {
+		print ("In calcDistance\n");
+		float distance2 = Vector3.Distance(origin.position, goal.position);
+		
+		return distance2;
+	}
+	
+	public float calcDistanceToWaypoint(Transform origin, Transform waypoint) {
+		print ("In calcDistanceToWaypoint\n");
+		float distance2 = Vector3.Distance(origin.position, waypoint.position);
+		
+		return distance2;
+	}
+}
