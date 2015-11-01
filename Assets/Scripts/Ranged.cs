902f57afbcd18756016a437235c14ebb3d70d749
kca8 <kca8@students.uwf.edu>
2015-10-31 20:51:37 -0500

    finished ranged script and combat demo; added some prefabs

--- Assets/Scripts/Ranged.cs
+++ Assets/Scripts/Ranged.cs
@@ -5,13 +5,16 @@ using System.Linq;
 public class Ranged : MonoBehaviour {
 
 	private Vector3 origin;
+	private GameObject myself;
 	private float timeOfLastShot = 0f;
 	private AIDirector ai;
 	
 	public float sight = 15f;
 	public float range = 15f;
-	public float rotationSpeed = 4f;
-	public float rateOfFire = 0.5f;
+	public float rotationSpeed = 10f;
+	public float reloadSpeed = 0.5f;
+	public float projSpeed = 25f;
+	public float timeToLive = 4f;
 	public bool directed = false;
 	public string[] tagsToTarget;
 	public Rigidbody projectile;
@@ -22,6 +25,7 @@ public class Ranged : MonoBehaviour {
 		ai = GameObject.FindObjectOfType<AIDirector>() as AIDirector;
 
 		origin = transform.position;
+		myself = gameObject;
 		if (range > sight) range = sight;
 	}
 	
@@ -29,8 +33,9 @@ public class Ranged : MonoBehaviour {
 	void Update () {
 		origin = transform.position;
 		Vector3 directionOfTarget = observeForTarget(origin, sight, tagsToTarget);
-		fire(directionOfTarget);
-	
+
+		float tempTime = fire (myself, directionOfTarget, projectile, reloadSpeed, timeOfLastShot, rotationSpeed, projSpeed, timeToLive, tagsToTarget);
+		if (tempTime >= 0) timeOfLastShot = tempTime;
 	}
 
 	public void GiveInfo(Collider seenObject){
@@ -95,16 +100,51 @@ public class Ranged : MonoBehaviour {
 		}
 	}
 
-	void fire (Vector3 directionOfTarget) {
+	// -------------------------------------------------------------------------
+	///@Keegan, Kirby, James
+	///@author Keegan Anderson
+	///<summary>
+	/// If supplied vector is nonzero, the GameObject "self" orients itself in the 
+	/// direction of the target at "rotSpeed". If the time passed since "lastShot" 
+	/// is greater than the "reload" delay, it will fire one "bullet" at 
+	/// "shotSpeed" with a time to live of "ttl" and set its tags to damage to 
+	/// "hostileTags". It then returns the current time to keep track of the time
+	/// since last shot.
+	/// 
+	/// If supplied vector is zero, or not enough time has expired, returns a 
+	/// sentinel value of -1
+	///</summary>
+	/// 
+	///<param name="self">>> GameObject to fire from and rotate</param>
+	///<param name="directionOfTarget">>> Vector3 orientation for projectile</param>
+	///<param name="bullet">>> Rigidbody of projectile to fire; should have a 
+	/// 	bullet.cs component</param>
+	///<param name="reload">>> delay between shots</param>
+	///<param name="lastShot">>> Time.time at last shot</param>
+	///<param name="rotSpeed">>> Speed at which object should rotate</param>
+	///<param name="shotSpeed">>> Speed at which bullet should travel</param>
+	///<param name="ttl">>> Time to live for bullet before self destroying</param> 
+	///<param name="hostileTags">>> array of strings to damage if collided with </param>
+	///<returns> Time.time if a shot if fired, -1 if shot isn't</returns>
+	///
+	// -------------------------------------------------------------------------
+	public static float fire (GameObject self, Vector3 directionOfTarget, Rigidbody bullet, float reload, float lastShot, float rotSpeed, float shotSpeed, float ttl, string[] hostileTags) {
 		if(directionOfTarget != Vector3.zero){
 
-			transform.forward = Vector3.RotateTowards(transform.forward, directionOfTarget, rotationSpeed * Time.deltaTime, 0.0f);
+			self.transform.forward = Vector3.RotateTowards(self.transform.forward, directionOfTarget, rotSpeed * Time.deltaTime, 0.0f);
 
-			if(Time.time > rateOfFire + timeOfLastShot){
-				Rigidbody tempProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
-				Physics.IgnoreCollision(tempProjectile.collider, gameObject.collider);
-				timeOfLastShot = Time.time;
+			if(Time.time > reload + lastShot){
+				Rigidbody tempProjectile = Instantiate(bullet, self.transform.position, self.transform.rotation) as Rigidbody;
+				Physics.IgnoreCollision(tempProjectile.collider, self.gameObject.collider);
+
+				tempProjectile.AddForce(tempProjectile.transform.forward * shotSpeed, ForceMode.Impulse);
+				tempProjectile.gameObject.GetComponent<Bullet>().setTags(hostileTags);
+				Destroy (tempProjectile.gameObject, ttl);
+
+				return Time.time;
 			}
+			
 		}
+		return -1;
 	}
 }
