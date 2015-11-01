2206f055eabedfb9b44f82e851968544db013b7f
khg4 <khg4@students.uwf.edu>
2015-11-01 14:41:50 -0600

    added the calculationStrat for AI director

--- Assets/Scripts/AIDirector.cs
+++ Assets/Scripts/AIDirector.cs
@@ -7,6 +7,11 @@ public class AIDirector : MonoBehaviour {
 	public float player_health;
 	public float NPC_health;
 	public string NPC_Type;
+	public float calc;
+	public float calc_ans;
+	public float ans;
+	public boolean fight;
+	public boolean run;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +22,38 @@ public class AIDirector : MonoBehaviour {
 	void Update () {
 	
 	}
+	// -------------------------------------------------------------------------
+	///@Keegan, Kirby, James
+	///@author Kirby Gagne
+	///<summary>
+	/// Takes the health of the player and NPC into account and calculates whether or not it should
+	/// attack or run from the player based on probablity calculations.
+	/// Sets either the fight or run local boolean variables to true when calling this function.
+	///</summary>
+	/// 
+	///
+	///
+	// -------------------------------------------------------------------------
+	public void CalculateStrat(){
+		fight = false;
+		run = false;
+		calc = player_health + NPC_health;
+		calc_ans = Random.Range (1, calc);
+		while (calc_ans > 100) {
+			ans = calc_ans - 100;
+			calc_ans = Random.Range (1, calc_ans);
+			ans = Random.Range (1, ans);
+			calc_ans = calc_ans - ans;
+		}
+		if (calc_ans >= 50) {
+			print ("over 50 fight should be true\n");
+			fight = true;
+		} else {
+			print ("under 50 run should be true\n");
+			run = true;
+		}
+	}
+
 
 	// -------------------------------------------------------------------------
 	///@Keegan, Kirby, James
@@ -70,17 +107,23 @@ public class AIDirector : MonoBehaviour {
 	///<summary>
 	///This function is receiving information from Director-controlled NPCs indicating all new visible
 	///Game Objects that the NPCs can react to. Given this information, the AI Director will decide on whether it would
-	///be better to pursue the new Game Object
+	///be better to pursue the new Game Object. Calls the function CalculateStrat if the boolean fight is true then
+	/// NPC's will persue the new Game Object.
 	///</summary>
 	/// 
 	///<param name="huntThisItem">>> a new target for the NPCs to hunt on the map
 	///
 	// -------------------------------------------------------------------------
 	public void setTargetForNPCs(string huntThisItem){
-		var objects = GameObject.FindGameObjectsWithTag("Hunter");
-		
-		foreach(var npc in objects){
-			npc.gameObject.GetComponent<Hunt>().addHuntTarget(huntThisItem);
+		CalculateStrat ();
+		if (fight == true) {
+			var objects = GameObject.FindGameObjectsWithTag("Hunter");
+			
+			foreach(var npc in objects){
+				npc.gameObject.GetComponent<Hunt>().addHuntTarget(huntThisItem);
+			}
+
 		}
+
 	}
 }
