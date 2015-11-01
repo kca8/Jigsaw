83d7d80dc658b7dfd19e8adc0ce88fd0e656a02f
khg4 <khg4@students.uwf.edu>
2015-11-01 15:36:48 -0600

    pushing again

--- Assets/Scripts/AIDirector.cs
+++ Assets/Scripts/AIDirector.cs
@@ -5,8 +5,13 @@ using System.Collections.Generic;
 public class AIDirector : MonoBehaviour {
 
 	public float player_health;
-	public int NPC_health;
-	public int NPC_Type;
+	public float NPC_health;
+	public string NPC_Type;
+	public int calc;
+	public int calc_ans;
+	public int ans;
+	public bool fight;
+	public bool run;
 
 	// Use this for initialization
 	void Start () {
@@ -17,25 +22,116 @@ public class AIDirector : MonoBehaviour {
 	void Update () {
 	
 	}
+	// -------------------------------------------------------------------------
+	///@Keegan, Kirby, James
+	///@author Kirby Gagne
+	///<summary>
+	/// Takes the health of the player and NPC into account and calculates whether or not it should
+	/// attack or run from the player based on probablity calculations.
+	/// Sets either the fight or run local boolean variables to true when calling this function.
+	///</summary>
+	/// 
+	///
+	///
+	// -------------------------------------------------------------------------
+	public void CalculateStrat(){
+		print ("inCalculate");
+		fight = false;
+		run = false;
+		calc = (int)player_health + (int)NPC_health;
+		print ("calc ans is\n");
+		print (calc_ans);
+		calc_ans = Random.Range (1, calc);
+		while (calc_ans > 100) {
+			ans = calc_ans - 100;
+			calc_ans = Random.Range (1, calc_ans);
+			ans = Random.Range (1, ans);
+			calc_ans = calc_ans - ans;
+		}
+		if (calc_ans >= 50) {
+			print ("over 50 fight should be true\n");
+			print ("calc ans is\n");
+			print (calc_ans);
+			fight = true;
+		} else {
+			print ("under 50 run should be true\n");
+			print ("calc ans is\n");
+			print (calc_ans);
+			run = true;
+		}
+	}
+
+
+	// -------------------------------------------------------------------------
+	///@Keegan, Kirby, James
+	///@author Kirby Gagne
+	///<summary>
+	///The health of the Player (or player controlled items) is sent to the AI Director
+	///for record keeping and for use in determining NPC strategies
+	///</summary>
+	/// 
+	///<param name="health">>> the health of the player
+	///
+	// -------------------------------------------------------------------------
 
 	public void setPlayerHealth(float health){
 		player_health = health;
 	}
 
-	public void getNPCHealth(int health){
+	// -------------------------------------------------------------------------
+	///@Keegan, Kirby, James
+	///@author Kirby Gagne
+	///<summary>
+	///The health of a particular NPC is passed to the AI Director to give the AI Director
+	///knowledge on the current status of Director-controlled NPCs
+	///</summary>
+	/// 
+	///<param name="health">>> the health of the NPC
+	///
+	// -------------------------------------------------------------------------
+	public void getNPCHealth(float health){
 		NPC_health = health;
 	}
 
-	public void getNPCType(int type){
+	// -------------------------------------------------------------------------
+	///@Keegan, Kirby, James
+	///@author James Rossel
+	///<summary>
+	///An NPCs particular type is sent to the AI Director for use in calulating strategies
+	///and tactics
+	///</summary>
+	/// 
+	///<param name="type">>> the NPC's type
+	///
+	// -------------------------------------------------------------------------
+	public void getNPCType(string type){
 		type = NPC_Type;
 	}
 	
+	// -------------------------------------------------------------------------
+	///@Keegan, Kirby, James
+	///@author James Rossel
+	///<summary>
+	///This function is receiving information from Director-controlled NPCs indicating all new visible
+	///Game Objects that the NPCs can react to. Given this information, the AI Director will decide on whether it would
+	///be better to pursue the new Game Object. Calls the function CalculateStrat if the boolean fight is true then
+	/// NPC's will persue the new Game Object.
+	///</summary>
+	/// 
+	///<param name="huntThisItem">>> a new target for the NPCs to hunt on the map
+	///
+	// -------------------------------------------------------------------------
 	public void setTargetForNPCs(string huntThisItem){
-		var objects = GameObject.FindGameObjectsWithTag("Hunter");
-		//List<Hunt> hunters = new List<Hunt>();
-		
-		foreach(var npc in objects){
-			npc.gameObject.GetComponent<Hunt>().addHuntTarget(huntThisItem);
+		print ("inSetTarget");
+		CalculateStrat ();
+		if (fight == true) {
+			var objects = GameObject.FindGameObjectsWithTag("Hunter");
+			
+			foreach(var npc in objects){
+				npc.gameObject.GetComponent<Hunt>().addHuntTarget(huntThisItem);
+			}
+
 		}
+
 	}
 }
